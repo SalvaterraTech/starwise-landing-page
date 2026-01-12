fffbc5643b9d0078e36499a82f6396d086ed04d7
Luiz Fernando Salvaterra <luizfernandosalvaterra@gmail.com>
2026-01-12 11:14:44 -0300

    refactor: testimonials section into reusable components
    Extracted testimonial badge and header markup from Testimonials.tsx into new TestimonialBadge and TestimonialHeader components for improved modularity and code reuse.

---
+++ components/testimonials/TestimonialBadge.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import React from "react";
+
+const TestimonialBadge = () => {
+    return (
+        <div
+            className="framer-6jqrb1"
+            data-border="true"
+            data-framer-name="Container"
+            style={{
+                "--border-bottom-width": "1px",
+                "--border-color":
+                    "var(--token-52be73dd-1bd3-4201-83f2-487c9f21864d, rgb(29, 29, 32))",
+                "--border-left-width": "1px",
+                "--border-right-width": "1px",
+                "--border-style": "solid",
+                "--border-top-width": "1px",
+                backgroundColor:
+                    "var(--token-89ca0e81-7337-4782-b669-5fecde3b876a, rgb(19, 19, 21))",
+                borderBottomLeftRadius: "100px",
+                borderBottomRightRadius: "100px",
+                borderTopLeftRadius: "100px",
+                borderTopRightRadius: "100px",
+            }}
+        >
+            <div className="framer-qquypx" data-framer-name="Icon">
+                <div
+                    style={{
+                        position: "absolute",
+                        borderRadius: "inherit",
+                        top: "0",
+                        right: "0",
+                        bottom: "0",
+                        left: "0",
+                    }}
+                    data-framer-background-image-wrapper="true"
+                >
+                    <img
+                        decoding="async"
+                        width="20"
+                        height="20"
+                        sizes="20px"
+                        src="https://framerusercontent.com/images/1CGcD1VdAFHtaEbo8XY5DBOwy5c.svg"
+                        alt="Icon"
+                        style={{
+                            display: "block",
+                            width: "100%",
+                            height: "100%",
+                            borderRadius: "inherit",
+                            objectPosition: "center",
+                            objectFit: "cover",
+                        }}
+                    />
+                </div>
+            </div>
+            <div
+                className="framer-edc4o"
+                data-framer-name="Text"
+                style={{
+                    outline: "none",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "flex-start",
+                    flexShrink: "0",
+                    "--framer-paragraph-spacing": "0px",
+                    transform: "none",
+                }}
+                data-framer-component-type="RichTextContainer"
+            >
+                <p
+                    className="framer-text framer-styles-preset-brsdai"
+                    data-styles-preset="o9ZJ7wliQ"
+                >
+                    Testimonials
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default TestimonialBadge;
---
+++ components/testimonials/TestimonialHeader.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import React from "react";
+
+const TestimonialHeader = () => {
+    return (
+        <div className="framer-1ewmzx7" data-framer-name="Text Container">
+            <div
+                className="framer-1c5axn3"
+                data-framer-name="Heading"
+                style={{
+                    outline: "none",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "flex-start",
+                    flexShrink: "0",
+                    "--framer-paragraph-spacing": "0px",
+                    transform: "none",
+                }}
+                data-framer-component-type="RichTextContainer"
+            >
+                <h2
+                    className="framer-text framer-styles-preset-6snecm"
+                    data-styles-preset="rVRByc2xi"
+                >
+                    Our Testimonials
+                </h2>
+            </div>
+            <div className="framer-dlymwt" data-framer-name="Paragraph Container">
+                <div
+                    className="framer-xl0q2a"
+                    data-framer-name="Paragraph"
+                    style={{
+                        outline: "none",
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "flex-start",
+                        flexShrink: "0",
+                        "--extracted-r6o4lv":
+                            "var(--token-1fe402aa-6f19-4409-814b-53c8bdf92f81, rgb(121, 123, 133))",
+                        "--framer-paragraph-spacing": "0px",
+                        transform: "none",
+                    }}
+                    data-framer-component-type="RichTextContainer"
+                >
+                    <p
+                        className="framer-text framer-styles-preset-11bw1qu"
+                        data-styles-preset="JkMvGTmRK"
+                        style={{
+                            "--framer-text-color":
+                                "var(--extracted-r6o4lv, var(--token-1fe402aa-6f19-4409-814b-53c8bdf92f81, rgb(121, 123, 133)))",
+                        }}
+                    >
+                        See how Starwise helps candidates prepare better and perform with
+                        confidence.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TestimonialHeader;
